61ece260151557ec93105e285881279c846b4106
nonaddict <jossefkisch@gmail.com>
2025-05-24 22:50:36 +0300

    adding homepage

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const response = fetch('https://sql-backend-production-6b3b.up.railway.app/get-all-products/display_products')
+.then(rawJson=>rawJson.json())
+.then(response=>{
+    
+const container=document.querySelector('.sample-products')
+
+for(let i = 0;i<6;i++){
+    let element=response.data[i]
+    
+    const article=document.createElement("article")
+    article.setAttribute("class","product-card")
+
+    const image=document.createElement("img")
+    image.setAttribute("src",element.image)
+
+    const name=document.createElement("h3")
+    name.innerHTML=element.name;
+
+    const price=document.createElement("p")
+    price.innerHTML=element.price+"$";
+
+    const link=document.createElement("a")
+    link.setAttribute("href",element.link)
+    link.setAttribute("target","_blank")
+    link.innerHTML="View on ebay"
+
+    article.appendChild(image)
+    article.appendChild(name)
+    article.appendChild(price)
+    article.appendChild(link)
+
+    container.appendChild(article)
+}
+})
+.catch(err=>console.log(err))
